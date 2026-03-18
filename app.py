0f8a477d0e3ff22e01b79e99c847b69e8984798d
Eddie <eddie@bloxid.com>
2026-03-18 13:50:42 +0000

    Revert to working simple version

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-# Hugging Face Spaces - REAL QUANTUM VERSION
-from quantum_terminal_demo import app
+# Hugging Face Spaces - Working Version
+from quantum_simple import app
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7860, debug=True)
